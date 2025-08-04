b353df832c396c18feab36e2977ac0e9bf23a939
Hassan Saleem <smuhammadhassan39@gmail.com>
2025-08-04 22:51:50 +0500

    Removed .env and added to .gitignore

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173, // important for Render
+    allowedHosts: 'all', // allow Render domain
   },
 });
